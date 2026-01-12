1f457a2662ba3b4529f0add436d53ef2b9c7a7f1
rqpedro <queirooz@hotmail.com>
2026-01-12 10:14:43 -0300

    Listagem de Produto

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -44,6 +44,30 @@ public class ProdutosDAO {
 
     public ArrayList<ProdutosDTO> listarProdutos() {
         
+        String sql = "SELECT * FROM produtos";
+
+        try {
+            conn = new conectaDAO().connectDB();
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+
+            listagem.clear();
+
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+
+                listagem.add(produto);
+            }
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar produtos: " + e.getMessage());
+        }
+        
         return listagem;
     }
 
