5edd60cbc24e4ad0cf32b334101809676b51e58b
rqpedro <queirooz@hotmail.com>
2026-01-13 08:15:11 -0300

    Criada a tela de Vendas, exibir produtos vendidos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -87,4 +87,32 @@ public class ProdutosDAO {
         }
     }
 
+    public ArrayList<ProdutosDTO> listarProdutosVendidos() {
+
+        String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+        ArrayList<ProdutosDTO> listaVendidos = new ArrayList<>();
+
+        try {
+            conn = new conectaDAO().connectDB();
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+
+                listaVendidos.add(produto);
+            }
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar produtos vendidos: " + e.getMessage());
+        }
+
+        return listaVendidos;
+    }
+
 }
